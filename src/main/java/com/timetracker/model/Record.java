1df35336fa673b9c48847e2a14fe5daa954f5ca9
Pawan Pugliya <goappsmart@gmail.com>
2021-07-18 23:28:26 +0530

    Integrated H2 database

--- src/main/java/com/timetracker/model/Record.java
+++ src/main/java/com/timetracker/model/Record.java
@@ -2,11 +2,21 @@ package com.timetracker.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Record {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
     private String email;
 
+    @Column
     private String start;
 
+    @Column
     private String end;
 }
--- src/main/java/com/timetracker/service/impl/TimeTrackerServiceImpl.java
+++ src/main/java/com/timetracker/service/impl/TimeTrackerServiceImpl.java
@@ -2,7 +2,9 @@ package com.timetracker.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.timetracker.model.Record;
+import com.timetracker.repository.TimeTrackerRepository;
 import com.timetracker.service.TimeTrackerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,29 +19,33 @@ import java.util.stream.Collectors;
 @Service
 public class TimeTrackerServiceImpl implements TimeTrackerService {
 
-    private List<Record> records;
-    private RestTemplate restTemplate;
+    @Autowired
+    private TimeTrackerRepository repository;
+
     private final String DOCKER_SERVICE_URL = "http://192.168.99.100:8080/";
+    private RestTemplate restTemplate;
     private ObjectMapper mapper;
 
     public TimeTrackerServiceImpl() {
-        records = new ArrayList<>();
         restTemplate = new RestTemplate();
         mapper = new ObjectMapper();
     }
 
     @Override
     public void saveTrackingDetails(Record record) {
+        System.out.println(record.toString());
         // Replace the - in date with . AND replace the T in time with space separator.
         record.setStart(record.getStart().replaceAll("-", "."));
         record.setStart(record.getStart().replaceAll("T", " "));
         record.setEnd(record.getEnd().replaceAll("-", "."));
         record.setEnd(record.getEnd().replaceAll("T", " "));
 
+        // NOTE: Saving a copy to local h2database as docker service is not reachable
+        repository.save(record);
         try {
             this.postTrackingDetailsToExternalService(DOCKER_SERVICE_URL + "/records", record);
         } catch(Exception e) {
-            records.add(record);
+            System.out.println(e);
         }
     }
 
@@ -51,31 +57,48 @@ public class TimeTrackerServiceImpl implements TimeTrackerService {
                 return this.getTrackingDetailsFromExternalService(DOCKER_SERVICE_URL + "/records?offset=0&length=10");
             }
             catch(Exception e) {
-                return records;
+                // NOTE: Get the top 10 tracking details from local H2 database for given email if the docker service is not available
+                return this.getTrackingDetailsFromLocalH2Database();
             }
         }
         try {
             return this.getTrackingDetailsFromExternalService(DOCKER_SERVICE_URL + "/records?email=" + emailAddress);
         } catch(Exception e) {
-            return records.stream().filter(item -> item.getEmail().equals(emailAddress)).collect(Collectors.toList());
+            // NOTE: Get top 10 tracking details from local H2 database for given email if the docker service is not available
+            return this.getTrackingDetailsForEmailFromLocalH2Database(emailAddress);
         }
     }
 
     private List<Record> getTrackingDetailsFromExternalService(String url) {
-        List<Record> records = new ArrayList<>();
         ResponseEntity<Object[]> recordsEntity = restTemplate.getForEntity(url, Object[].class);
 
         if(recordsEntity.getStatusCode() == HttpStatus.OK) {
-            records = Arrays.stream(recordsEntity.getBody())
+            return Arrays.stream(recordsEntity.getBody())
                     .map(object -> mapper.convertValue(object, Record.class))
                     .collect(Collectors.toList());
         }
-
-        return records;
+        return null;
     }
 
     private void postTrackingDetailsToExternalService(String url, Record record) {
         HttpEntity<Record> recordEntity = new HttpEntity<>(record);
         restTemplate.postForEntity(url, recordEntity, Record.class);
+    }
+
+    private List<Record> getTrackingDetailsForEmailFromLocalH2Database(String emailAddress) {
+        ArrayList<Record> recordsForEmail = new ArrayList<>();
+        repository.findAll().forEach(recordItem -> {
+            if(recordItem.getEmail().equals(emailAddress)) {
+                recordsForEmail.add(recordItem);
+            }
+        });
+        return recordsForEmail.stream().limit(10).collect(Collectors.toList());
+    }
+
+    private List<Record> getTrackingDetailsFromLocalH2Database() {
+        ArrayList<Record> allRecords = new ArrayList<>();
+        repository.findAll().forEach(record -> allRecords.add(record));
+
+        return allRecords.stream().limit(10).collect(Collectors.toList());
     }
 }
